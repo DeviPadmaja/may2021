75019cf380d2ef63d7b020c0463aa47918ac6515
root <root@localhost.localdomain>
2021-06-02 04:35:28 -0400

    1st commit

---
+++ project1.java
@@ -0,0 +1,6 @@
+class jenkins{
+    public static void main(String[] args) {
+        System.out.println("welcom to jenkins");
+	System.out.println("lets start executing this project1");	 
+    }
+ }
